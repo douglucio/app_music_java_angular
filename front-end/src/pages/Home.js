4b3212bb1879f644b1f8e06c76b9922de7d5a798
douglucio <ronaldoreis.web@gmail.com>
2023-04-01 12:44:30 -0300

    atualizando arquivos de front-end

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>Bem-vindo ao Meu Site de Músicas</h1>
       <p>Esta é a página inicial do site.</p>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/users">Listar usuarios</Link>
+        </li>
+      </ul>
     </div>
   );
 };
